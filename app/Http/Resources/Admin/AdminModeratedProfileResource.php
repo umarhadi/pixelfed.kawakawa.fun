6fc2dd07d4eb679e0401d12510c0c961305071c7
Daniel Supernault <danielsupernault@gmail.com>
2024-10-15 04:31:09 -0600

    Add AdminModeratedProfileResource.php

---
+++ app/Http/Resources/Admin/AdminModeratedProfileResource.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Resources\Admin;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use App\Profile;
+
+class AdminModeratedProfileResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        $profileObj = [];
+        $profile = Profile::withTrashed()->find($this->profile_id);
+        if($profile) {
+            $profileObj = [
+                'username' => $profile->username,
+            ];
+        }
+        return [
+            'id' => $this->id,
+            'domain' => $this->domain,
+            'profile' => $profileObj,
+            'profile_id' => $this->profile_id,
+            'profile_url' => $this->profile_url,
+            'note' => $this->note,
+            'is_banned' => (bool) $this->is_banned,
+            'is_nsfw' => (bool) $this->is_nsfw,
+            'is_unlisted' => (bool) $this->is_unlisted,
+            'is_noautolink' => (bool) $this->is_noautolink,
+            'is_nodms' => (bool) $this->is_nodms,
+            'is_notrending' => (bool) $this->is_notrending,
+            'created_at' => $this->created_at,
+        ];
+    }
+}
